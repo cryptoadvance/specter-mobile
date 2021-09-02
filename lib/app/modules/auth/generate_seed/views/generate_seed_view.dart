7d6267021737f2f3c18f7e89eaff056249cb9cc7
Alexandr <test@test.example>
2021-09-02 23:11:32 +0300

    Generate seed screen

--- lib/app/modules/auth/generate_seed/views/generate_seed_view.dart
+++ lib/app/modules/auth/generate_seed/views/generate_seed_view.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:specter_mobile/app/modules/auth/generate_seed/widgets/GeneratedSeedList.dart';
+import 'package:specter_mobile/app/widgets/LightButton.dart';
 
 import '../controllers/generate_seed_controller.dart';
 
@@ -8,12 +11,116 @@ class GenerateSeedView extends GetView<GenerateSeedController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'GenerateSeedView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: Text('Your recovery phrase', style: TextStyle(fontSize: 20)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  color: Colors.red.withOpacity(0.3),
+                  borderRadius: BorderRadius.all(Radius.circular(5))
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Text('Write it down and never show to anybody!', style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: GeneratedSeedList()
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: getControlComplexityPanel()
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: getBottomButtonsPanel()
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+
+  Widget getControlComplexityPanel() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Expanded(
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Use 24 words', style: TextStyle(fontSize: 16)),
+                Text(
+                  'Generates a 24-word seed phrase containing 256 bits of entropy instead of 128 in a 12-word variant.',
+                  style: TextStyle(color: Colors.grey[300]),
+                ),
+              ],
+            )
+          )
         ),
-      ),
+        Expanded(
+          flex: 0,
+          child: Container(
+            child: Text('[C]'),
+          )
+        )
+      ]
+    );
+  }
+
+  Widget getBottomButtonsPanel() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          child: LightButton(
+            child: Row(
+              children: [
+                Icon(CupertinoIcons.back),
+                Container(
+                  margin: EdgeInsets.only(left: 5),
+                  child: Text('BACK')
+                )
+              ]
+            ),
+            style: LightButtonStyle.WHITE_OUTLINE,
+            onTap: () {
+              Get.back();
+            }
+          )
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 10),
+          child: LightButton(
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 5),
+                  child: Text('DONE')
+                ),
+                Icon(CupertinoIcons.check_mark)
+              ]
+            ),
+            style: LightButtonStyle.PRIMARY,
+            onTap: () {
+
+            }
+          ),
+        )
+      ]
     );
   }
 }
--- lib/app/modules/auth/recovery_select/views/recovery_select_view.dart
+++ lib/app/modules/auth/recovery_select/views/recovery_select_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
 import 'package:specter_mobile/app/modules/auth/generate_seed/views/generate_seed_view.dart';
@@ -10,46 +11,57 @@ class RecoverySelectView extends GetView<RecoverySelectController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              child: Text('recovery_select_labels_top_title', style: TextStyle(fontSize: 20)),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 30),
-              child: Text('recovery_select_labels_middle_help'.tr)
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.only(top: 30),
-              child: LightButton(
-                child: Text('recovery_select_buttons_generate_new_seed'.tr, textAlign: TextAlign.center),
-                isInline: false,
-                onTap: () {
-                  Get.to(GenerateSeedView(), arguments: {
-                  });
-                }
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  shape: BoxShape.circle
+                ),
+                padding: EdgeInsets.all(20),
+                child: SvgPicture.asset('assets/icons/vpn_key.svg', color: Colors.grey[600]),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.only(top: 10),
-              child: LightButton(
-                child: Text('recovery_select_buttons_recovery_phrase'.tr, textAlign: TextAlign.center),
-                isInline: false,
-                style: LightButtonStyle.WHITE_OUTLINE,
-                onTap: () {
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Text('recovery_select_labels_top_title'.tr, style: TextStyle(fontSize: 20)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: Text('recovery_select_labels_middle_help'.tr)
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 30),
+                child: LightButton(
+                  child: Text('recovery_select_buttons_generate_new_seed'.tr, textAlign: TextAlign.center),
+                  isInline: false,
+                  onTap: () {
+                    Get.to(GenerateSeedView(), arguments: {
+                    });
+                  }
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.only(top: 10),
+                child: LightButton(
+                  child: Text('recovery_select_buttons_recovery_phrase'.tr, textAlign: TextAlign.center),
+                  isInline: false,
+                  style: LightButtonStyle.WHITE_OUTLINE,
+                  onTap: () {
 
-                }
-              ),
-            )
-          ],
+                  }
+                )
+              )
+            ]
+          )
         )
-      ),
+      )
     );
   }
 }
