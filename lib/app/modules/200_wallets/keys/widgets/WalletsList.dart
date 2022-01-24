50ff968abc4119939494bb6b90fe9e5b2bb309fa
Alexander <test@test.test>
2022-01-24 15:48:55 +0300

    Update wallets screen

--- lib/app/modules/200_wallets/keys/widgets/WalletsList.dart
+++ lib/app/modules/200_wallets/keys/widgets/WalletsList.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'AccountItem.dart';
+import 'WalletItem.dart';
 
 class WalletsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 15,
-      padding: EdgeInsets.all(0),
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: AccountItem()
-        );
-      }
+    return SliverList(
+        delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+          return Container(
+              margin: EdgeInsets.only(top: (index == 0)?15:0, left: 15, right: 15, bottom: 15),
+              child: WalletItem()
+          );
+        },
+        childCount: 2
+      )
     );
   }
 }
