6797b3a4f0981b290b07f818f2db8af4a1f2113b
Alexander <4531547+AleksMx@users.noreply.github.com>
2022-03-13 19:40:24 +0300

    Test rust storage

--- specter_rust/rust_storage/src/main.rs
+++ specter_rust/rust_storage/src/main.rs
@@ -1,18 +1,16 @@
+#![allow(non_snake_case)]
+
 pub mod specter_storage;
 
-use std::ffi::{CString, CStr};
+use std::ffi::{CString};
 
 fn main() {
     println!("Hello, world!");
 
-    unsafe { 
-        let path = CString::new( "/home/linux-dev/Documents/flutter-apps/other/tmp".to_owned()).unwrap().into_raw();
-        let y = specter_storage::open_storage(path);
+    let path = CString::new( "/home/linux-dev/Documents/flutter-apps/other/tmp".to_owned()).unwrap().into_raw();
+    let isOk = specter_storage::open_storage(path);
 
-        let slice = CStr::from_ptr(y);
-        let sv = slice.to_string_lossy().into_owned();
-        
-        println!("{}", sv);
-    }
-    
+    let c_to_print = CString::new("test").expect("CString::new failed");
+
+    specter_storage::write_storage(0, 0, c_to_print.as_ptr(), 4);
 }
--- specter_rust/rust_storage/src/specter_storage/mod.rs
+++ specter_rust/rust_storage/src/specter_storage/mod.rs
@@ -1,11 +1,13 @@
 #![allow(unused_imports)]
+#![allow(non_snake_case)]
 
 use std::os::raw::{c_char};
 use std::ffi::{CString, CStr};
+use std::ptr;
 use std::str::FromStr;
 
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, SeekFrom, Seek};
 use std::path::Path;
 
 struct DiskHeaderVolume {
@@ -20,24 +22,85 @@ unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
     )
 }
 
-#[no_mangle]
-pub extern fn open_storage(path: *const c_char) -> *mut c_char {
-    let pathStr: &CStr = unsafe { CStr::from_ptr(path) };
-    let dataDir: String = pathStr.to_str().unwrap().to_owned();
-    let filePath = dataDir + "/header.raw";
-    let usedVolumes = 5;
+struct DiskStorage {
+     dataDir: &'static str
+}
+impl DiskStorage {
+    pub fn open_storage(&mut self, path: *const c_char) -> bool {
+        let pathStr: &CStr = unsafe { CStr::from_ptr(path) };
+        let str_slice: &str = pathStr.to_str().unwrap();
 
-    if !Path::new(filePath.as_str()).exists() {
-        let mut file = File::create(filePath.as_str()).expect("Unable to open");
+        let filePath;
+        self.dataDir = str_slice;
+        filePath = self.dataDir.to_string() + "/header.raw";
 
-        for volumeIdx in 0..usedVolumes {
-            let my_struct = DiskHeaderVolume { volumeIndex: volumeIdx, reserved: [0; 128]};
-            let bytes: &[u8] = unsafe { any_as_u8_slice(&my_struct) };
-            file.write_all(bytes);
+        print!("Open header: {}\n", filePath);
+        
+        const USED_VOLUMES: u8 = 5;
+    
+        if !Path::new(filePath.as_str()).exists() {
+            let mut file = File::create(filePath.as_str()).expect("Unable to open");
+    
+            for volumeIdx in 0..USED_VOLUMES {
+                let my_struct = DiskHeaderVolume { volumeIndex: volumeIdx, reserved: [0; 128]};
+                let bytes: &[u8] = unsafe { any_as_u8_slice(&my_struct) };
+                file.write_all(bytes);
+            }
+    
+            print!("Header file created\n");
         }
-
-        print!("Header file created\n");
+    
+        return true;
     }
 
-    CString::new("ok").unwrap().into_raw()
+    pub fn write_storage(&mut self, volumeIdx: i32, clusterIdx: i32, data: *const c_char, dataSize: i32) -> bool {
+        let filePath;
+        filePath = self.dataDir.to_string() + &"/data_".to_string() + &clusterIdx.to_string() + ".raw";
+
+        print!("Open storage: {}\n", filePath);
+
+        const USED_VOLUMES: u8 = 5;
+        const CLUSTER_SIZE: usize = 1024;
+
+        if !Path::new(filePath.as_str()).exists() {
+            let mut file = File::create(filePath.as_str()).expect("Unable to open");
+    
+            for volumeIdx in 0..USED_VOLUMES {
+                let bytes: [u8; CLUSTER_SIZE] = [0; CLUSTER_SIZE];
+                file.write_all(&bytes);
+            }
+
+            print!("Data file created\n");
+        }
+
+        let mut file = File::create(filePath.as_str()).expect("Unable to open");
+        let offsetStart = volumeIdx * CLUSTER_SIZE as i32;
+        file.seek(SeekFrom::Start(offsetStart as u64)).expect("Unable to write to file");
+
+        let mut bytes: [u8; CLUSTER_SIZE] = [0; CLUSTER_SIZE];
+
+        file.write_all(&bytes);
+        
+        return true;
+    }
+}
+static mut diskStorage: DiskStorage = DiskStorage {
+    dataDir: ""
+};
+
+#[no_mangle]
+pub extern fn open_storage(path: *const c_char) -> bool {
+    unsafe {
+        return diskStorage.open_storage(path);
+    }
+}
+
+pub extern fn read_storage(volumeIdx: *const i32, data: *const c_char, cluster_idx: i32) -> bool {
+    return true;
+}
+
+pub extern fn write_storage(volumeIdx: i32, clusterIdx: i32, data: *const c_char, dataSize: i32) -> bool {
+    unsafe {
+        return diskStorage.write_storage(volumeIdx, clusterIdx, data, dataSize);
+    }
 }
